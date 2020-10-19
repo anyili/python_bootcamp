8bae8918a331193575f88583f28d230bba289a9b
Anyi Li <lia5@mskcc.org>
2020-10-19 01:27:56 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Python bootcamp
+
+### hello.py
+hello world python code
+### basic.py
+basic syntax
+
+ 
---
+++ src/basic.py
@@ -0,0 +1,56 @@
+"""
+    Copyright (c) 2019, 2020, Memorial Sloan Kettering Cancer Center (MSKCC)
+    All rights reserved.
+    Unauthorized copying of this file, via any medium is strictly prohibited.
+    MSKCC PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ 
+"""
+
+# variable and type
+
+age = 20
+name = 'python'
+is_patient = True
+
+print(age)
+print(name)
+print(is_patient)
+
+# dict
+person = {
+    'age': 20,
+    'name': 'python',
+    'is_patient': False
+}
+print(person['age'])
+
+# list
+teams = ['Team A', 'Team B', 'Team R']
+print(teams[2])
+print(teams[3])
+
+# control
+
+if len(teams) > 1:
+    print('more than 1 team')
+if 'Team B' in teams:
+    print('there is Team B')
+
+# loop
+for team in teams:
+    print(team)
+
+# list comprehension
+
+teams_lower = [team.lower() for team in teams]
+for team in teams_lower:
+    print(team)
+     print(team + '?')
+
+
+# method
+def reformat(p):
+    str_formatted = 'name: {}, age: {}'.format(p['name'], p['age'])
+    return str_formatted
+
+print(reformat(person))
